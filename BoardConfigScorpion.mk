9687b307d7bbf01777c3d48c8580b312c9796134
Danny Lin <danny@kdrag0n.dev>
2020-11-02 19:43:49 -0800

    redfin: Enable refresh rate toggle in Settings
    This device supports switching between 60 and 90 Hz refresh rates, so
    expose it in Settings -> Display -> Smooth Display for users to save
    battery if necessary.
    
    Test: visual confirmation after toggling several times
    Change-Id: Ie698ec4d4e738afd2a9055dba2369233103a4f13

--- BoardConfigScorpion.mk
+++ BoardConfigScorpion.mk
@@ -4,9 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Add before redbull BoardConfigLineage.mk
+# Add before redbull BoardConfigScorpion.mk
 BOOT_KERNEL_MODULES += sec_touch.ko
 
-include device/google/redbull/BoardConfigLineage.mk
+include device/google/redbull/BoardConfigScorpion.mk
 
 include vendor/google/redfin/BoardConfigVendor.mk
--- device-scorpion.mk
+++ device-scorpion.mk
@@ -5,3 +5,6 @@
 #
 
 $(call inherit-product, device/google/redbull/device-scorpion.mk)
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/redfin/overlay-scorpion
