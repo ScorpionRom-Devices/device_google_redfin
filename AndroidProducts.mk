c3b5fa5ebd1e35beda64cbcf279a94cde39893fa
Craig Gomez <craigacgomez@gmail.com>
2020-11-29 20:05:16 -0800

    DU-ify and add du.dependencies

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,7 +15,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_redfin.mk \
-
-COMMON_LUNCH_CHOICES := \
-    aosp_redfin-userdebug \
+    $(LOCAL_DIR)/aosp_redfin.mk
---
+++ du.mk
@@ -0,0 +1,51 @@
+# Copyright (C) 2018-2020 The Dirty Unicorns Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file is the build configuration for an aosp Android
+# build for marlin hardware. This cleanly combines a set of
+# device-specific aspects (drivers) with a device-agnostic
+# product configuration (apps). Except for a few implementation
+# details, it only fundamentally contains two inherit-product
+# lines, aosp and du, hence its name.
+#
+
+# Include DU common configuration
+include vendor/du/config/common_full_phone.mk
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/google/redfin/aosp_redfin.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+PRODUCT_NAME := du_redfin
+PRODUCT_DEVICE := redfin
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 5
+PRODUCT_SYSTEM_BRAND := google
+PRODUCT_SYSTEM_MANUFACTURER := Google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="redfin" \
+    PRIVATE_BUILD_DESC="redfin-user 11 RD1A.201105.003.C1 6886399 release-keys"
+
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RD1A.201105.003.C1/6886399:user/release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=google/redfin/redfin:11/RD1A.201105.003.C1/6886399:user/release-keys
+
+$(call inherit-product-if-exists, vendor/google/redfin/redfin-vendor.mk)
+$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
